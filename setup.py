c8791a1dd8745e583efffc504e1c2161689dc50f
Roger Howard <rogerhoward@mac.com>
2017-10-17 15:31:54 -0700

    Fix package name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-iiif',
+    name='django_iiif',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
