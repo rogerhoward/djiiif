20c197e4ed2dc305e4d94ec18181cc23cdf5bcd1
Roger Howard <rogerhoward@mac.com>
2017-12-04 21:44:13 -0800

    check class of parameter

--- djiiif/templatetags/iiiftags.py
+++ djiiif/templatetags/iiiftags.py
@@ -3,10 +3,14 @@ from django.db.models import ImageField
 from django.db.models.fields.files import ImageFieldFile
 from django.conf import settings
 from django import template 
+from djiiif import IIIFObject
+
 register = template.Library()
 
 
 @register.simple_tag
 def iiif(imagefield, profile):
-    return(getattr(imagefield, profile))
-
+    if isinstance(imagefield, IIIFObject):
+        return(getattr(imagefield.iiif, profile))
+    else:
+        return None
