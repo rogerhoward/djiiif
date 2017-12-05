48448b2df508e9792a4beca821d0b332057d466c
Roger Howard <rogerhoward@mac.com>
2017-12-05 07:55:06 -0800

    Add custom exception

--- djiiif/templatetags/iiiftags.py
+++ djiiif/templatetags/iiiftags.py
@@ -5,9 +5,17 @@ from django.conf import settings
 from django import template 
 from djiiif import IIIFObject
 
+
+class NotAnIIIFField(AttributeError):
+    def __init__(self, *args, **kwargs):
+        AttributeError.__init__(self, *args, **kwargs)
+
 register = template.Library()
 
 
 @register.simple_tag
 def iiif(imagefield, profile):
-    return getattr(imagefield.iiif, profile)
+    try:
+        return getattr(imagefield.iiif, profile)
+    except AttributeError:
+        raise NotAnIIIFField('The iiif template tag expects an instance of a IIIFField as its first parameter.')
